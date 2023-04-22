8464af9f82586768a7a534d56b6de3fe46d1b6a0
Emily Iannizzi <eiannizzi18@gmail.com>
2023-04-22 15:30:06 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  module.exports = {
+    matchName
+  };
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function (driver) {
+      return driver.startsWith(letters);
+    });
+  }
+  
+  module.exports = {
+    fuzzyMatch,
+  };
+  
